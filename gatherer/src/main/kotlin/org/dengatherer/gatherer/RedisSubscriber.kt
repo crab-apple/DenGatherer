359412739f9ed382793226660d4eb192484cac0e
crab-apple <andreu.bastardas@gmail.com>
2021-04-13 00:13:01 +0200

    Redis subscriber class that prints the exposes to console as they come

---
+++ gatherer/src/main/kotlin/org/dengatherer/gatherer/RedisSubscriber.kt
@@ -0,0 +1,22 @@
+package org.dengatherer.gatherer
+
+import redis.clients.jedis.Jedis
+import redis.clients.jedis.JedisPubSub
+
+class RedisSubscriber {
+    fun subscribe() {
+        val jedis = Jedis()
+        jedis.subscribe(MyListener(), "exposes")
+    }
+}
+
+class MyListener : JedisPubSub() {
+    override fun onMessage(channel: String?, message: String?) {
+        println("Received message on channel $channel")
+        println(message)
+    }
+}
+
+fun main(args: Array<String>) {
+    RedisSubscriber().subscribe()
+}
